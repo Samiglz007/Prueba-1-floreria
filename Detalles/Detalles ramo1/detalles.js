ba8c02006f69a4613df02575ce3efae72b668a0b
Samiglz007 <sam007hernan@gmail.com>
2026-04-24 09:14:29 -0600

    Actualización de Detalles

---
+++ Detalles/Detalles ramo1/detalles.js
@@ -0,0 +1,5 @@
+function agregarAlCarrito() {
+    const producto = document.querySelector('h1').innerText;
+    alert(`¡Genial! El "${producto}" se ha añadido a tu carrito.`);
+    // Aquí podrías guardar en LocalStorage en el futuro
+}
